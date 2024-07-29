a90a9fc503a6fc0e61e394a8c5017038ae4b6c37
mamtapoojary13 <108426661+mamtapoojary13@users.noreply.github.com>
2024-07-29 22:48:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // proxy requests prefixed '/api' and '/uploads'
     proxy: {
-      "/api": "http://localhost:5000",
-      "/uploads": "http://localhost:5000",
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/uploads": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
+  build: {
+    outDir: "../dist", // Adjust this path to specify where you want the build output to go
+  },
 });
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://localhost:5000",
-//         changeOrigin: true,
-//         secure: false, // This can help if the backend server is using self-signed SSL
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//       "/uploads": {
-//         target: "http://localhost:5000",
-//         changeOrigin: true,
-//         secure: false,
-//         rewrite: (path) => path.replace(/^\/uploads/, ''),
-//       },
-//     },
-//   },
-// });
