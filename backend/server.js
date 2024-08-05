7adc32d98853b60c6e88591328143177aa20848c
mamtapoojary13 <108426661+mamtapoojary13@users.noreply.github.com>
2024-08-05 18:54:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -10,17 +9,12 @@ import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const port = process.env.PORT || 5000;
 
 connectDB(); //Connect to MongoDB
 
 const app = express();
-
-app.use(cors({
-  origin: 'https://ecommerce-frontend-aiva.onrender.com', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
 //Body parser middleware
 app.use(express.json());
@@ -28,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 
 //Cookie parser middleware
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
